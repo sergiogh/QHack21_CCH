9148a1b54e9986cc48d2e3c7562f627362436ebb
inigoliz <inilariz@gmail.com>
2021-02-18 16:22:50 +0100

    simple_30_....

--- solutions/simple_circuits_30_sol.py
+++ solutions/simple_circuits_30_sol.py
@@ -4,37 +4,38 @@ from pennylane import numpy as np
 import sys
 
 
-def simple_circuits_20(angle):
+def simple_circuits_30(angle):
     """The code you write for this challenge should be completely contained within this function
-    between the # QHACK # comment markers.
+        between the # QHACK # comment markers.
 
     In this function:
-        * Rotate the qubit around the x-axis by angle
-        * Measure the probability the qubit is in the zero state
+        * Rotate a qubit around the y-axis by angle
+        * Measure the expectation value of `PauliX`
 
     Args:
-        angle (float): how much to rotate a state around the x-axis
+        angle (float): how much to rotate a state around the y-axis
 
     Returns:
-        float: the probability of of the state being in the 0 ground state
+        float: the expectation value of a PauliX measurement
     """
-    prob = 0.0
+    x_expectation = 0.0
+
     # QHACK #
 
-    # Step 1 : initalize a device
+    # Step 1 : initialize a device
     dev = qml.device("default.qubit", wires=1)
 
     # Step 2 : Create a quantum circuit and qnode
     @qml.qnode(dev)
     def circuit(angle):
-        qml.RX(angle, wires=0)
-        return qml.probs(wires=0)
+        qml.RY(angle, wires=0)
+        return qml.expval(qml.PauliX(0))
 
     # Step 3 : Run the qnode
-    prob = circuit(angle)[0]  # just get the prob. of |0>
+    x_expectation = circuit(angle)
 
     # QHACK #
-    return prob
+    return x_expectation
 
 
 if __name__ == "__main__":
@@ -44,12 +45,12 @@ if __name__ == "__main__":
     angle_str = sys.stdin.read()
     angle = float(angle_str)
 
-    ans = simple_circuits_20(angle)
+    ans = simple_circuits_30(angle)
 
     if isinstance(ans, np.tensor):
         ans = ans.item()
 
     if not isinstance(ans, float):
-        raise TypeError("the simple_circuits_20 function needs to return a float")
+        raise TypeError("the simple_circuits_30 function needs to return a float")
 
     print(ans)
